99c77cc70cc5003053bf0f0021fb8be5e418b5f3
Mark <mark.edmondson@cellpointmobile.com>
2023-07-07 11:36:53 +0200

    refactor slack_config

--- slack/app.py
+++ slack/app.py
@@ -5,94 +5,28 @@ sys.path.append(parent_dir)
 
 import uvicorn
 from fastapi import FastAPI, Request
-import logging
-from webapp import bot_help
 from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
-import aiohttp
-import asyncio
+import slack_help
 
 app = AsyncApp()
 app_handler = AsyncSlackRequestHandler(app)
 
 api = FastAPI()
 
-async def send_to_qa_async(user_input, vector_name, chat_history):
-
-    qna_url = os.getenv('QNA_URL', None)
-    if qna_url is None:
-       raise ValueError('QNA_URL not found in environment')
-
-    qna_endpoint = f'{qna_url}/qna/{vector_name}'
-    qna_data = {
-        'user_input': user_input,
-        'chat_history': chat_history,
-    }
-    logging.info(f"Sending to {qna_endpoint} this data: {qna_data}")
-    
-    # this breaks it? Its sending it multiple times though
-    async with aiohttp.ClientSession() as session:
-        async with session.post(qna_endpoint, json=qna_data) as resp:
-            qna_response = await resp.json()
-
-    logging.info(f"Got back QA response: {qna_response}")
-    return qna_response
-
-async def process_slack_message(sapp, body, logger, thread_ts=None):
-    logger.info(body)
-    logging.info("Calling async process_slack_message")
-    team_id = body.get('team_id', None)
-    if team_id is None:
-        raise ValueError('Team_id not specified')
-    user_input = body.get('event').get('text','').strip()
-
-    user = body.get('event').get('user')
-    bot_user = body.get('authorizations')[0].get('user_id')
-
-    bot_mention = f"<@{bot_user}>"
-    user_input = user_input.replace(bot_mention, '').strip()
-
-    vector_name = bot_help.get_slack_vector_name(team_id, bot_user)
-    if vector_name is None:
-        raise ValueError(f'Could not derive vector_name from slack_config and {team_id}, {bot_user}')
-    
-    logging.debug(f'Slack vector_name: {vector_name}')
-
-    chat_historys = await sapp.client.conversations_replies(
-        channel=body['event']['channel'],
-        ts=thread_ts
-    ) if thread_ts else await sapp.client.conversations_history(
-        channel=body['event']['channel']
-    )
-
-    messages = chat_historys['messages']
-
-    command_response = bot_help.handle_special_commands(user_input, vector_name, messages)
-    if command_response is not None:
-        return command_response['result']
-    
-
-    logging.info(f'Sending from Slack: {user_input} to {vector_name}')
-    bot_output = await send_to_qa_async(user_input, vector_name, chat_history=messages)
-    logging.info(f"Slack bot_output: {bot_output}")
-
-    slack_output = bot_output.get("answer", "No answer available")
-
-    return slack_output
-
 @app.event("app_mention")
 async def handle_app_mention(ack, body, say, logger):
     await ack() 
     logger.info("app_mention")
     thread_ts = body['event']['ts']
-    slack_output = await process_slack_message(app, body, logger, thread_ts)
+    slack_output = await slack_help.process_slack_message(app, body, logger, thread_ts)
     await say(text=slack_output)
 
 @app.event("message")
 async def handle_direct_message(ack, body, say, logger):
     await ack()
     logger.info("message")
-    slack_output = await process_slack_message(app, body, logger)
+    slack_output = await slack_help.process_slack_message(app, body, logger)
     await say(text=slack_output)
 
 @api.post('/slack/message')
---
+++ slack/slack_help.py
@@ -0,0 +1,89 @@
+import logging
+import os
+import json
+import bot_help
+
+async def send_to_qa_async(user_input, vector_name, chat_history):
+
+    qna_url = os.getenv('QNA_URL', None)
+    if qna_url is None:
+       raise ValueError('QNA_URL not found in environment')
+
+    qna_endpoint = f'{qna_url}/qna/{vector_name}'
+    qna_data = {
+        'user_input': user_input,
+        'chat_history': chat_history,
+    }
+    logging.info(f"Sending to {qna_endpoint} this data: {qna_data}")
+    
+    # this breaks it? Its sending it multiple times though
+    async with aiohttp.ClientSession() as session:
+        async with session.post(qna_endpoint, json=qna_data) as resp:
+            qna_response = await resp.json()
+
+    logging.info(f"Got back QA response: {qna_response}")
+    return qna_response
+
+async def process_slack_message(sapp, body, logger, thread_ts=None):
+    logger.info(body)
+    logging.info("Calling async process_slack_message")
+    team_id = body.get('team_id', None)
+    if team_id is None:
+        raise ValueError('Team_id not specified')
+    user_input = body.get('event').get('text','').strip()
+
+    user = body.get('event').get('user')
+    bot_user = body.get('authorizations')[0].get('user_id')
+
+    bot_mention = f"<@{bot_user}>"
+    user_input = user_input.replace(bot_mention, '').strip()
+
+    vector_name = slack_help.get_slack_vector_name(team_id, bot_user)
+    if vector_name is None:
+        raise ValueError(f'Could not derive vector_name from slack_config and {team_id}, {bot_user}')
+    
+    logging.debug(f'Slack vector_name: {vector_name}')
+
+    chat_historys = await sapp.client.conversations_replies(
+        channel=body['event']['channel'],
+        ts=thread_ts
+    ) if thread_ts else await sapp.client.conversations_history(
+        channel=body['event']['channel']
+    )
+
+    messages = chat_historys['messages']
+
+    command_response = bot_help.handle_special_commands(user_input, vector_name, messages)
+    if command_response is not None:
+        return command_response['result']
+    
+
+    logging.info(f'Sending from Slack: {user_input} to {vector_name}')
+    bot_output = await send_to_qa_async(user_input, vector_name, chat_history=messages)
+    logging.info(f"Slack bot_output: {bot_output}")
+
+    slack_output = bot_output.get("answer", "No answer available")
+
+    return slack_output
+
+def load_config(filename):
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    parent_dir = os.path.dirname(script_dir)
+
+    # Join the script directory with the filename
+    config_path = os.path.join(parent_dir, filename)
+
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    return config
+
+slack_config = load_config('slack/slack_config.json')
+
+def get_slack_vector_name(team_id, bot_user):
+    logging.info(f'getting slack vector_name: {team_id} - {bot_user}')
+    try:
+        return slack_config['team_ids'][team_id]['bot_users'][bot_user]['llm']
+    except KeyError:
+        logging.error('Could not find slack config')
+        return None
