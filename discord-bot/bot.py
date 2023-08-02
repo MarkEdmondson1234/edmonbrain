b9121e795dc48273c7a9953b19ff2978d5df08bf
Mark <mark.edmondson@cellpointmobile.com>
2023-08-02 14:18:25 +0200

    improve chat history to always satart with human

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -70,19 +70,24 @@ async def make_chat_history(new_thread, bot_mention, client_user):
             continue
         history.append(msg)
 
-    print(f"history: {history}")
-
     # Reverse the messages to maintain the order of conversation
     chat_history = []
-    for msg in reversed(history):
+    for i in reversed(range(len(history))):
+        msg = history[i]
         author = "AI" if msg.author == client_user else "Human"
         clean_content = msg.content.replace(bot_mention, '').strip()
         embeds = [embed.to_dict() for embed in msg.embeds]
+        # Skip the last human message
+        if i == len(history)-1 and author == "Human":
+            continue
+        # Start from a human message
+        if i < len(history)-1 and chat_history == [] and author == "AI":
+            continue
         chat_history.append({"name": author, "content": clean_content, "embeds": embeds})
 
-    print(f"chat history: {chat_history}")
-
+    print(f"chat_history: {chat_history}")
     return chat_history
+
 
 async def make_new_thread(message, clean_content):
     # Check if the message was sent in a thread or a private message
