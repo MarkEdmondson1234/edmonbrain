44af46fc0121ef447fba01ec3963a7d26253547b
Mark <mark.edmondson@cellpointmobile.com>
2023-08-18 18:18:54 +0200

    take out vector stuff

--- qna/agent.py
+++ qna/agent.py
@@ -15,19 +15,9 @@ from httpx import ReadTimeout
 import logging
 import traceback
 
-def activate_agent(question, chat_history, vector_name, llm_chat):
+def activate_agent(question, chat_history, llm_chat):
 
-    _, embeddings, llm = pick_llm(vector_name)
-
-    vectorstore = pick_shared_vectorstore(vector_name, embeddings)
-
-    search = RetrievalQA.from_chain_type(llm=llm_chat, chain_type="stuff", retriever=vectorstore.as_retriever())
     tools = [
-        # Tool( 
-        #     name="memory-search",
-        #     description="useful for when you are searching your memory for more context to your questions",
-        #     func=search.run
-        # ),  # get rate limited having too many tools
         Tool(
             name="python-repl",
             description="useful for when you need to calculate something using programing. Use print() to see the answer.",
@@ -36,8 +26,6 @@ def activate_agent(question, chat_history, vector_name, llm_chat):
     ]
 
     #memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    #llm = ChatOpenAI(temperature=0)
-        # only openai supports streaming for now
     max_retries=1
     initial_delay = 5
     import time
